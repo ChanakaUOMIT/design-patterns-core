13ad40134e4a1fd32848b65f8a286cb9b6e0676a
ChanakaUOMIT <chanakauomfit@gmail.com>
2020-12-09 13:30:55 +0530

    inheritance and polymorphism concept

--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -3,26 +3,46 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
+        var MessageIndicator = new MessageIndicator();
         //class concept
+        MessageIndicator.topicDeclaration("Class Concept");
 	    User user = new User("Chanaka");
         user.sayHello();
 
         //interface concept
+        MessageIndicator.topicDeclaration("Interface Concept");
         TaxCalculator calculator= getCalculator();
-        calculator.calculateTax();
+        float rate=calculator.calculateTax();
+        System.out.println(rate);
 
         //encapsulation concept
+        MessageIndicator.topicDeclaration("Encapsulation Concept");
         var account = new Account();
         account.deposit(10);
         account.withdraw(5);
         System.out.println(account.getBalance());
 
         //abstract concept
+        MessageIndicator.topicDeclaration("Abstract Concept");
         var mailService = new MailService();
         mailService.sendEmail();
+
+        //inheritance concept
+        MessageIndicator.topicDeclaration("Inheritance Concept");
+        var textBox= new TextBox();
+        textBox.enable();
+
+        //polymorphism concept
+        MessageIndicator.topicDeclaration("Polymorphism Concept");
+        drawUIControl(new TextBox());
+        drawUIControl(new CheckBox());
     }
 
     public static TaxCalculator getCalculator(){
         return new TaxCalculator2019();
+    }
+
+    public static void drawUIControl(UIControl control){
+        control.draw();
     }
 }
---
+++ src/com/learning/MessageIndicator.java
@@ -0,0 +1,7 @@
+package com.learning;
+
+public class MessageIndicator {
+    public void topicDeclaration(String message){
+        System.out.println("-------- "+message+" ---------");
+    }
+}
